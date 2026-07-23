127fc90eaabe730e9f229b554a13655603f7b0ae
Keerthi Kumar V <kumarkeerthi442@gmail.com>
2026-07-23 19:37:58 +0530

    Fix socket.io routing and auth-service cookies

--- services/auth-service/src/middleware/authMiddleware.js
+++ services/auth-service/src/middleware/authMiddleware.js
@@ -2,7 +2,16 @@ const jwt = require('jsonwebtoken');
 
 // Protect routes
 exports.protect = async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
 
   // Make sure token exists
   if (!token) {
